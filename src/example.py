df611c9b2f0180e94b0753f2d72ea59c3060bbcb
andrea <andrea_ramazzina@hotmail.it>
2023-01-22 11:43:36 +0100

    update library

--- src/example.py
+++ src/example.py
@@ -2,14 +2,14 @@ from instagram_interface import Instagram_I_IH
 
 
 # Get a free token at www.influencerhunters.com
-TOKEN = "XXXXXXXXXXXX"
+TOKEN = "YOUR TOKEN HERE"
 
 #Initialize sender class 
 tt = Instagram_I_IH(token_IH_API=TOKEN)
 
 #Send the request to the IH server
 print("sending the request..")
-res, success = tt.get_posts_from_username("fedez")
+res, success = tt.get_user_posts_from_username(username="cristiano", depth=1, oldest_timestamp=1611308425 )
 
 
 if success:
